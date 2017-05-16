7ae1a476cd8e9bc1d030196974927c335fc6fc72
guiduan <254732500@qq.com>
2017-05-16 11:59:33 +0800

    user表结构

---
+++ database/migrations/2017_05_10_081616_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInstancesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->integer('enable')->default(1);
+            $table->decimal('money', 12, 2);
+            $table->string('password');
+            $table->integer('is_admin')->default(0);
+            $table->integer('verified')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user');
+    }
+
+}
